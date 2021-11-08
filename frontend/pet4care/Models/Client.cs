a1d3eb0f16d2db6cb6e03405f16483c9af90f437
Negikami <45533155+Negikami@users.noreply.github.com>
2021-11-08 15:54:23 +0100

    Add files via upload

---
+++ frontend/pet4care/Models/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pet4care.Models
+{
+    public class Client
+    {
+        public int Id { get; private set; }
+        public String Nom { get; private set; }
+        public String Prenom { get; private set; }
+        public String Adresse { get; private set; }
+        public String Tel { get; private set; }
+        public String Mdp { get; private set; }
+    }
+}

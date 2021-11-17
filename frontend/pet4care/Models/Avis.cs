c4a29c57989c3491f24c7ae1929cbd27f8492e55
ClementLOTH <91614233+ClementLOTH@users.noreply.github.com>
2021-11-17 17:13:04 +0100

    Ajout de la Liste des Gardiens

---
+++ frontend/pet4care/Models/Avis.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pet4care.Models
+{
+    public class Avis
+    {
+        public int Id { get; set; }
+        public String Nom { get; set; }
+        public String Message { get; set; }
+
+        private int _note;
+        public int Note
+        {
+            get => _note;
+            set
+            {
+                if ((value >= 0) && (value <= 5))
+                {
+                    _note = value;
+                }
+            }
+        }
+
+
+    }
+}
---
+++ frontend/pet4care/Models/Gardien.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pet4care.Models
+{
+    public class Gardien : Client
+    {
+        public Boolean Status { get; set; }
+        public List<Avis> ListAvis { get; set; }
+        // public List<DateTime> Disponibilites { get; set; }
+    }
+}

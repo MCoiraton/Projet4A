56703599889025b14c7ffe898a6e1381823cfe94
ClementLOTH <91614233+ClementLOTH@users.noreply.github.com>
2021-12-08 17:41:46 +0100

    Add files via upload

---
+++ backend/pet4care/src/test/java/fr/pet4care/details/Pet4careApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.pet4care;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pet4careApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

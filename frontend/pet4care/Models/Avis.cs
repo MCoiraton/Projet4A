6ff26964a5d62d18f3dfb4a5955f67d284a482b3
ClementLOTH <91614233+ClementLOTH@users.noreply.github.com>
2022-01-11 16:54:18 +0100

    Update Avis.cs

--- frontend/pet4care/Models/Avis.cs
+++ frontend/pet4care/Models/Avis.cs
@@ -7,23 +7,9 @@ namespace pet4care.Models
 {
     public class Avis
     {
-        public int Id { get; set; }
-        public String Nom { get; set; }
+        public int IdGardien { get; set; }
+        public int IdAuteur { get; set; }
+        public int Note { get; set; }
         public String Message { get; set; }
-
-        private int _note;
-        public int Note
-        {
-            get => _note;
-            set
-            {
-                if ((value >= 0) && (value <= 5))
-                {
-                    _note = value;
-                }
-            }
-        }
-
-
     }
 }

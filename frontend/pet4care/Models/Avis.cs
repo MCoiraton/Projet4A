89e4616c6b442eb13a51c04b2c4fd1f235402fdb
Negikami <45533155+Negikami@users.noreply.github.com>
2022-01-11 19:50:42 +0100

    Update Avis.cs

--- frontend/pet4care/Models/Avis.cs
+++ frontend/pet4care/Models/Avis.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System;
 
 namespace pet4care.Models
 {
     public class Avis
     {
+        [JsonProperty("idGardien")]
         public int IdGardien { get; set; }
+        [JsonProperty("idAuteur")]
         public int IdAuteur { get; set; }
+        [JsonProperty("note")]
         public int Note { get; set; }
+        [JsonProperty("message")]
         public String Message { get; set; }
     }
 }

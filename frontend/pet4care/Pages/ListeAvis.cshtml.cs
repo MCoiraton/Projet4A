95a7e24bf42eba0db950ba75c6d76d9fa78e2a34
ClementLOTH <91614233+ClementLOTH@users.noreply.github.com>
2022-01-11 15:32:08 +0100

    Update ListeAvis.cshtml.cs

--- frontend/pet4care/Pages/ListeAvis.cshtml.cs
+++ frontend/pet4care/Pages/ListeAvis.cshtml.cs
@@ -1,120 +1,25 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using pet4care.Models;
+using pet4care.Services;
 
 namespace pet4care.Pages
 {
     public class ListeAvisModel : PageModel
     {
         public List<Avis> Aviss;
-        public void OnGet()
+        public IActionResult OnGet()
         {
-            Aviss = new List<Avis>();
-            Aviss.Add(new Avis()
-            {
-                Id = 55,
-                Nom = "Thomas BoBlond",
-                Message= "C'est trop cool ce système, wow, j'adore ce gars en plus.C'est trop cool ce système, wow, j'adore ce gars en plusC'est trop cool ce système, wow, j'adore ce gars en plusC'est trop cool ce système, wow, j'adore ce gars en plusC'est trop cool ce système, wow, j'adore ce gars en plusC'est trop cool ce système, wow, j'adore ce gars en plusC'est trop cool ce système, wow, j'adore ce gars en plusC'est trop cool ce système, wow, j'adore ce gars en plusC'est trop cool ce système, wow, j'adore ce gars en plusC'est trop cool ce système, wow, j'adore ce gars en plusC'est trop cool ce système, wow, j'adore ce gars en plusC'est trop cool ce système, wow, j'adore ce gars en plusC'est trop cool ce système, wow, j'adore ce gars en plusC'est trop cool ce système, wow, j'adore ce gars en plusC'est trop cool ce système, wow, j'adore ce gars en plusC'est trop cool ce système, wow, j'adore ce gars en plusC'est trop cool ce système, wow, j'adore ce gars en plus",
-                Note = 5 
-
-            });
-            Aviss.Add(new Avis()
-            {
-                Id = 55,
-                Nom = "Thomas BoBlond",
-                Message = "C'est trop cool ce système, wow, j'adore ce gars en plus.",
-                Note = 5
-
-            });
-            Aviss.Add(new Avis()
-            {
-                Id = 55,
-                Nom = "Thomas BoBlond",
-                Message = "C'est trop cool ce système, wow, j'adore ce gars en plus.",
-                Note = 5
-
-            });
-            Aviss.Add(new Avis()
-            {
-                Id = 55,
-                Nom = "Thomas BoBlond",
-                Message = "C'est trop cool ce système, wow, j'adore ce gars en plus.",
-                Note = 5
-
-            });
-            Aviss.Add(new Avis()
-            {
-                Id = 55,
-                Nom = "Thomas BoBlond",
-                Message = "C'est trop cool ce système, wow, j'adore ce gars en plus.",
-                Note = 5
-
-            });
-            Aviss.Add(new Avis()
-            {
-                Id = 55,
-                Nom = "Thomas BoBlond",
-                Message = "C'est trop cool ce système, wow, j'adore ce gars en plus.",
-                Note = 5
-
-            });
-            Aviss.Add(new Avis()
-            {
-                Id = 55,
-                Nom = "Thomas BoBlond",
-                Message = "C'est trop cool ce système, wow, j'adore ce gars en plus.",
-                Note = 5
-
-            });
-            Aviss.Add(new Avis()
-            {
-                Id = 55,
-                Nom = "Thomas BoBlond",
-                Message = "C'est trop cool ce système, wow, j'adore ce gars en plus.",
-                Note = 5
-
-            });
-            Aviss.Add(new Avis()
-            {
-                Id = 55,
-                Nom = "Thomas BoBlond",
-                Message = "C'est trop cool ce système, wow, j'adore ce gars en plus.",
-                Note = 5
-
-            });
-            Aviss.Add(new Avis()
-            {
-                Id = 55,
-                Nom = "Thomas BoBlond",
-                Message = "C'est trop cool ce système, wow, j'adore ce gars en plus.",
-                Note = 5
-
-            });
-            Aviss.Add(new Avis()
-            {
-                Id = 55,
-                Nom = "Thomas BoBlond",
-                Message = "C'est trop cool ce système, wow, j'adore ce gars en plus.",
-                Note = 5
-
-            });
-            Aviss.Add(new Avis()
-            {
-                Id = 55,
-                Nom = "Thomas BoBlond",
-                Message = "C'est trop cool ce système, wow, j'adore ce gars en plus.",
-                Note = 5
-
-            });
-
-
+            int id = int.Parse(RouteData.Values["id"].ToString());
+            var t = User.Claims.FirstOrDefault(x => x.Type == "Token");
+            if (t == null)
+                return RedirectToPage("/Login");
+            string token = t.Value;
+            AvisService aService = new AvisService(token);
+            Aviss = aService.GetByAuteurId(id);
+            return Page();
         }
-
-
-
     }
 }

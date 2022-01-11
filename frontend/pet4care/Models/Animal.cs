4d93c4c3808f79b84fe752a8bb2feb6ef5279ea3
Negikami <45533155+Negikami@users.noreply.github.com>
2022-01-11 11:05:19 +0100

    Update Animal.cs

--- frontend/pet4care/Models/Animal.cs
+++ frontend/pet4care/Models/Animal.cs
@@ -1,13 +1,11 @@
-﻿namespace pet4care.Models
+namespace pet4care.Models
 {
     public class Animal
     {
-        public string name { get; set; }
-
+        public int id { get; set; }
+        public int idProprietaire { get; set; }
+        public string nom { get; set; }
         public string type { get; set; }
-
-        public string espece { get; set; }
-
- 
+        public string description { get; set; }
     }
 }
